602c104592029408ff3ac227bd5a7a2d890ef361
IsaacLK <50344947+IsaacLK@users.noreply.github.com>
2021-07-02 17:29:56 -0400

    Update the bot

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import os
+import discord
+import random
+from discord.ext import commands
 
+#fill with discord bot token
+token = ""
+client = commands.Bot(command_prefix='/')
+@client.event
+async def on_ready():
+    print(f'{client.user} now online')
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='for /help | https://github.com/IsaacLK/opensourcediscordbot'))
+    print("code rcan")
+@client.command()
+async def hello(ctx):    
+    await ctx.send("Hello!")
+
+client.run(token)
