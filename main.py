2583bcc344117b22ab6676af1e3755a855c0a02f
IsaacLK <50344947+IsaacLK@users.noreply.github.com>
2021-07-04 12:24:06 -0400

    Update main.py
    Now using Environment Variables (.env) to store the bot's token

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import os
 import discord
 import random
 from discord.ext import commands
+from dotenv import load_dotenv
 
+load_dotenv()
 #fill with discord bot token
-token = ""
+
+token = os.getenv('TOKEN')
+
+
 client = commands.Bot(command_prefix='/')
 @client.event
 async def on_ready():
     print(f'{client.user} now online')
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='for /help | https://github.com/IsaacLK/opensourcediscordbot'))
-    print("code rcan")
+    
 @client.command()
 async def hello(ctx):    
     embed=discord.Embed(title="Hello!", color=0x003b46)
